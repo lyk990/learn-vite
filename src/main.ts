21333f1a2037c3f14e9633f579c852b15c556e79
lyk990 <1477435414@qq.com>
2023-03-31 00:03:10 +0800

    add image

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import fib from "virtual:fib";
 
-createApp(App).mount('#app')
+alert(`结果: ${fib(10)}`);
+alert(fib);
+createApp(App).mount("#app");
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import myVirtualVitePlugin from "./src/plugins/myPlugin";
+import inspect from "vite-plugin-inspect";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [vue(), myVirtualVitePlugin(), inspect()],
+});
